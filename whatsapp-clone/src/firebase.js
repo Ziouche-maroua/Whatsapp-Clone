57544a805ec79c6024bbd4ba431a40ab93e33b74
Ziouche Maroua <142220454+Ziouche-maroua@users.noreply.github.com>
2024-07-05 16:46:34 +0100

    Delete whatsapp-clone directory (plan changed)

--- whatsapp-clone/src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyChv31BDXe1dZIfk58NE_z3m2ffiSxSD1Q",
-    authDomain: "whatsapp-mern-9d33d.firebaseapp.com",
-    projectId: "whatsapp-mern-9d33d",
-    storageBucket: "whatsapp-mern-9d33d.appspot.com",
-    messagingSenderId: "312498232147",
-    appId: "1:312498232147:web:f83db8b59c2fc49e22dfa6"
-  };
